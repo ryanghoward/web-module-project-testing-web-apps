34344ee0d95aa1c46eaa4c075ec3669c624e54fc
Ryan Howard <ryanghoward@protonmail.com>
2022-01-13 11:54:15 -0800

    Test 3 passing

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -20,7 +20,13 @@ test('renders the contact form header', ()=> {
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
-    
+  render(<ContactForm />);
+  
+  const firstName = screen.getByLabelText(/first name/i);
+  userEvent.type(firstName, 'abc');
+
+  const errorMessages = await screen.findAllByTestId('error');
+  expect(errorMessages).toHaveLength(1);
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
